7a80fc5faad30c0981dac8b1d33b46bacc532d31
TanguyCazalets <tanguy.cazalets@ecl14.ec-lyon.fr>
2018-03-01 15:14:03 +0100

    ok

--- dessin.js
+++ dessin.js
@@ -57,6 +57,10 @@ var nodesGroup = svgD.append("g")
 var t = d3.transition()
 	.duration(750);
 
+// Panel E : Butons
+
+var svgE = d3.select(".e svg");
+
 // Data
 
 d3.csv("data/nutriments.csv", function (data) {
@@ -451,13 +455,13 @@ function displayBarChart() {
 		.attr("x", widthC - 19)
 		.attr("width", 19)
 		.attr("height", 19)
-		.attr("fill", colors)
+		.attr("fill", colors2)
 
 	legend.append("text")
 		.attr("x", widthC - 24)
 		.attr("y", 9.5)
 		.attr("dy", "0.32em")
-		.text(function (d) { return d; });*/
+		.text(function (d) { return d; });
 
 	console.log(selectedPlants);
 
